f7da6d6d193f8b65ca4e86d8546b6ed6fc2b71ac
Tesmi-Devlop <sashdfr@gmail.com>
2024-09-14 17:14:27 +1000

    feat: added more types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { $reflect } from "@rbxts/reflection-runtime";
 export * from "@rbxts/reflection-runtime";
 
-$reflect<string | number | boolean | thread>();
+type RobloxTypes = Omit<CheckableTypes, keyof CheckablePrimitives>;
+type Primitives = Omit<CheckablePrimitives, "nil" | "userdata">;
+
+$reflect<RobloxTypes[keyof RobloxTypes] | Primitives[keyof Primitives]>();
